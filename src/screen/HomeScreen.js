89feb84b6a1cd111b566595b47705815be5da8a9
Ryan B <ryan.brilliant.nirwana@gmail.com>
2020-10-04 19:09:03 +0700

    Adding product screeen

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -7,11 +7,11 @@ import Product from "../components/Product";
 const HomeScreen = () => {
   return (
     <>
-      <h1>Our Latest Product</h1>
+      <h1>Products</h1>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <Product key={product._id} product={product} />
+          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
--- src/screen/ProductScreen.js
+++ src/screen/ProductScreen.js
@@ -1,7 +1,92 @@
 import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import {
+  Col,
+  Row,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  Breadcrumb,
+  ListGroupItem,
+  Container,
+} from "react-bootstrap";
+import Rating from "../components/Rating";
 
-const ProductScreen = () => {
-  return <div>Product Screen</div>;
+import products from "../products";
+
+const ProductScreen = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id);
+
+  return (
+    <>
+      <Breadcrumb>
+        <LinkContainer to="/">
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+        </LinkContainer>
+        <Breadcrumb.Item active>{product.name}</Breadcrumb.Item>
+      </Breadcrumb>
+
+      <Container>
+        <Row>
+          <Col className="product-image" md={6}>
+            <Image src={product.image} fluid></Image>
+          </Col>
+          <Col className="product-details" md={3}>
+            <ListGroup variant="flush">
+              <ListGroupItem>
+                <h3 className="prod-name">{product.name}</h3>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Rating
+                  value={product.rating}
+                  text={` ${product.numReviews} Reviews`}
+                />
+              </ListGroupItem>
+              <ListGroupItem>
+                <p className="prod-desc">{product.description} </p>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroupItem>
+                  <Row>
+                    <Col>
+                      <strong>Price : &nbsp;</strong>
+                    </Col>
+                    <Col>$ {product.price}</Col>
+                  </Row>
+                </ListGroupItem>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Stock : &nbsp;</Col>
+                    <Col>
+                      <strong>
+                        {product.countInStock > 0
+                          ? "Alvailable"
+                          : "Out Of Stock"}
+                      </strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Button
+                    variant="primary"
+                    disabled={product.countInStock <= 0}
+                    block
+                  >
+                    Add To Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default ProductScreen;
