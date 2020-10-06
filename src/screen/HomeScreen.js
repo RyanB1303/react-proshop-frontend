eeecb40e22d4e63dc933302c2bad7f0d7153eb8d
Ryan B <ryan.brilliant.nirwana@gmail.com>
2020-10-06 13:04:55 +0700

    implement redux for fetch data

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,31 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
-
-// import products from "../products";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
 import Product from "../components/Product";
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  //const [products, setProducts] = useState([]);  // tidak dibutuhkan jika sudah menggunakan redux
+
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      let response = await fetch("/api/products");
-      const data = await response.json(); // ini harus di json kan , karena kita pakai fetch api javascript bukan axios, di axios otomatis jadi json
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h3>Loading....</h3>
+      ) : error ? (
+        <h4 style={{ color: "red" }}>{error}</h4>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
